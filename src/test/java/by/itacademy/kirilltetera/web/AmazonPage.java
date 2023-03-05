bafc5e3405a2ef513cc66cf33b0c7caeb5fe3540
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-03-05 17:51:52 +0300

    add AmazonPage java class

---
+++ src/test/java/by/itacademy/kirilltetera/web/AmazonPage.java
@@ -0,0 +1,6 @@
+package by.itacademy.kirilltetera.web;
+
+public class AmazonPage {
+    public static final String url = "https://www.amazon.com/";
+    public static final String copyright = "/html/body/div[1]/div[5]/div[5]/span";
+}
