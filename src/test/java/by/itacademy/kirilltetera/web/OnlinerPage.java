5bab75f3d2e7c17613183576fe25e3aa7db3f4dc
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-03-05 16:43:37 +0300

    add OnlinerPage java class

---
+++ src/test/java/by/itacademy/kirilltetera/web/OnlinerPage.java
@@ -0,0 +1,15 @@
+package by.itacademy.kirilltetera.web;
+public class OnlinerPage {
+    public static final String url = "https://www.onliner.by/";
+    public static final String copyright = "//div[@class='footer-style__copy']";
+    public static final String buttonEntrance = "//div[@class='auth-bar__item auth-bar__item--text']";
+    public static final String textEntrance =
+            "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String buttonEnter =
+            "//button[@class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
+    public static final String warningMessageEmptyNickOrEmail =
+            "/html/body/div[6]/div/div/div/div/div/div/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String warningMessageEmptyPassword =
+            "/html/body/div[6]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String inputNickOrEmail = "//input[@placeholder='Ник или e-mail']";
+}
