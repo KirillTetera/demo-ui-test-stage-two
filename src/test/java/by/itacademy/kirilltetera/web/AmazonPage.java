84b5f88188497c0b8539ee5e202e77c3089b9434
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-03-05 21:14:29 +0300

    refactor AmazonPage java class

--- src/test/java/by/itacademy/kirilltetera/web/AmazonPage.java
+++ src/test/java/by/itacademy/kirilltetera/web/AmazonPage.java
@@ -3,4 +3,12 @@ package by.itacademy.kirilltetera.web;
 public class AmazonPage {
     public static final String url = "https://www.amazon.com/";
     public static final String copyright = "/html/body/div[1]/div[5]/div[5]/span";
+    public static final String cartButton = "//a[4][@id='nav-cart']";
+    public static final String cartIsEmptyWarning = "//div[@class='a-row sc-your-amazon-cart-is-empty']";
+    public static final String signInButton = "//a[@id='a-autoid-0-announce']";
+    public static final String signInText = "//h1[@class='a-spacing-small']";
+    public static final String continueButton = "//input[@aria-labelledby='continue-announce']";
+    public static final String warningEmptyCredentials =
+            "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div[1]/form/div/div/div/div[1]/div/div/div";
+
 }
