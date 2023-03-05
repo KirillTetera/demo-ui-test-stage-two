f2814c25feac32dd42599393b8272b45064d27dc
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-03-05 16:44:08 +0300

    add OnlinerTest java class

---
+++ src/test/java/by/itacademy/kirilltetera/web/OnlinerTest.java
@@ -0,0 +1,70 @@
+package by.itacademy.kirilltetera.web;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class OnlinerTest {
+    WebDriver driver;
+    @Before
+    public void preporation(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to(OnlinerPage.url);
+    }
+    @Test
+    public void testOnlinerCopyright(){
+        WebElement copyright = driver.findElement(By.xpath(OnlinerPage.copyright));
+        Assert.assertEquals("© 2001—2023 Onlíner", copyright.getText());
+    }
+    @Test
+    public void testOpenOnlinerLoginForm(){
+        WebElement buttonEntrance = driver.findElement(By.xpath(OnlinerPage.buttonEntrance));
+        buttonEntrance.click();
+        WebElement textEntrance = driver.findElement(By.xpath(OnlinerPage.textEntrance));
+        Assert.assertEquals("Вход", textEntrance.getText());
+    }
+    @Test
+    public void testOnlinerLoginFormWithEmptyCredentials(){
+        WebElement buttonEntrance = driver.findElement(By.xpath(OnlinerPage.buttonEntrance));
+        buttonEntrance.click();
+        WebElement buttonEnter = driver.findElement(By.xpath(OnlinerPage.buttonEnter));
+        buttonEnter.click();
+        WebElement warningMessageEmptyNickOrEmail = driver.findElement
+                (By.xpath(OnlinerPage.warningMessageEmptyNickOrEmail));
+        Assert.assertEquals("Укажите ник или e-mail", warningMessageEmptyNickOrEmail.getText());
+        WebElement warningMessageEmptyPassword = driver.findElement
+                (By.xpath(OnlinerPage.warningMessageEmptyPassword));
+        Assert.assertEquals("Укажите пароль", warningMessageEmptyPassword.getText());
+    }
+    @Test
+    public void testOnlinerLoginFormWithEmptyPassword() {
+        WebElement buttonEntrance = driver.findElement
+                (By.xpath(OnlinerPage.buttonEntrance));
+        buttonEntrance.click();
+        WebElement inputNickOrEmail = driver.findElement
+                (By.xpath(OnlinerPage.inputNickOrEmail));
+        inputNickOrEmail.sendKeys("test@gmail.com");
+        WebElement buttonEnter = driver.findElement
+                (By.xpath(OnlinerPage.buttonEnter));
+        buttonEnter.click();
+        WebElement warningMessageEmptyPassword = new WebDriverWait(driver, Duration.ofSeconds(1))
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath(OnlinerPage.warningMessageEmptyPassword)));
+        Assert.assertEquals("Укажите пароль", warningMessageEmptyPassword.getText());
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
