470627df5f954fad1fa40054062123fae2a58781
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-03-09 08:03:25 +0300

    refactor OnlinerPage java class refactor OnlinerTest java class

--- src/test/java/by/itacademy/kirilltetera/web/OnlinerTest.java
+++ src/test/java/by/itacademy/kirilltetera/web/OnlinerTest.java
@@ -7,61 +7,56 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.List;
 
 public class OnlinerTest {
     WebDriver driver;
+
     @Before
-    public void preporation(){
+    public void preporation() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.navigate().to(OnlinerPage.url);
     }
+
     @Test
-    public void testOnlinerCopyright(){
-        WebElement copyright = driver.findElement(By.xpath(OnlinerPage.copyright));
-        Assert.assertEquals("© 2001—2023 Onlíner", copyright.getText());
+    public void testOnlinerCopyright() {
+        OnlinerStep onlinerStep = new OnlinerStep(driver);
+        onlinerStep.testOnlinerCopyright();
+        Assert.assertEquals("© 2001—2023 Onlíner",
+                driver.findElement(By.xpath(OnlinerPage.copyright)).getText());
     }
+
     @Test
-    public void testOpenOnlinerLoginForm(){
-        WebElement buttonEntrance = driver.findElement(By.xpath(OnlinerPage.buttonEntrance));
-        buttonEntrance.click();
-        WebElement textEntrance = driver.findElement(By.xpath(OnlinerPage.textEntrance));
-        Assert.assertEquals("Вход", textEntrance.getText());
+    public void testOpenOnlinerLoginForm() {
+        OnlinerStep onlinerStep = new OnlinerStep(driver);
+        onlinerStep.testOpenOnlinerLoginForm();
+        Assert.assertEquals("Вход",
+                driver.findElement(By.xpath(OnlinerPage.textEntrance)).getText());
     }
+
     @Test
-    public void testOnlinerLoginFormWithEmptyCredentials(){
-        WebElement buttonEntrance = driver.findElement(By.xpath(OnlinerPage.buttonEntrance));
-        buttonEntrance.click();
-        WebElement buttonEnter = driver.findElement(By.xpath(OnlinerPage.buttonEnter));
-        buttonEnter.click();
-        List<WebElement> warningMessagesEmptyCredentials = driver.findElements(
-                By.xpath(OnlinerPage.warningMessagesEmptyCredentials));
+    public void testOnlinerLoginFormWithEmptyCredentials() {
+        OnlinerStep onlinerStep = new OnlinerStep(driver);
+        onlinerStep.testOnlinerLoginFormWithEmptyCredentials();
+        List<WebElement> warningMessagesEmptyCredentials =
+                driver.findElements(By.xpath(OnlinerPage.warningMessagesEmptyCredentials));
         Assert.assertEquals("Укажите ник или e-mail", warningMessagesEmptyCredentials.get(0)
                 .getText());
         Assert.assertEquals("Укажите пароль", warningMessagesEmptyCredentials.get(1)
                 .getText());
     }
+
     @Test
     public void testOnlinerLoginFormWithEmptyPassword() {
-        WebElement buttonEntrance = driver.findElement
-                (By.xpath(OnlinerPage.buttonEntrance));
-        buttonEntrance.click();
-        WebElement inputNickOrEmail = driver.findElement
-                (By.xpath(OnlinerPage.inputNickOrEmail));
-        inputNickOrEmail.sendKeys("test@gmail.com");
-        WebElement buttonEnter = driver.findElement
-                (By.xpath(OnlinerPage.buttonEnter));
-        buttonEnter.click();
-        WebElement warningMessagesEmptyCredentials = driver.
-                findElement(By.xpath(OnlinerPage.warningMessagesEmptyCredentials));
-        Assert.assertEquals("Укажите пароль", warningMessagesEmptyCredentials.getText());
+        OnlinerStep onlinerStep = new OnlinerStep(driver);
+        onlinerStep.testOnlinerLoginFormWithEmptyPassword();
+        Assert.assertEquals("Укажите пароль",
+                driver.findElement(By.xpath(OnlinerPage.warningMessagesEmptyCredentials)).getText());
     }
+
     @After
     public void tearDown() {
         driver.quit();
