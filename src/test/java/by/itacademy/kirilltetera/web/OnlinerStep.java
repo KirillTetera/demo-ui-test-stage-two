a5745c72c2086a6bf7bac1a7296db6b155df8b86
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-03-09 08:02:29 +0300

    add OnlinerStep java class

---
+++ src/test/java/by/itacademy/kirilltetera/web/OnlinerStep.java
@@ -0,0 +1,43 @@
+package by.itacademy.kirilltetera.web;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class OnlinerStep {
+    WebDriver driver;
+    OnlinerStep (WebDriver driver) {
+        this.driver=driver;
+    }
+    public void testOnlinerCopyright() {
+        WebElement copyright = driver.findElement(By.xpath(OnlinerPage.copyright));
+    }
+    public void testOpenOnlinerLoginForm() {
+        WebElement buttonEntrance = driver.findElement(By.xpath(OnlinerPage.buttonEntrance));
+        buttonEntrance.click();
+        WebElement textEntrance = driver.findElement(By.xpath(OnlinerPage.textEntrance));
+    }
+
+    public void testOnlinerLoginFormWithEmptyCredentials(){
+        WebElement buttonEntrance = driver.findElement(By.xpath(OnlinerPage.buttonEntrance));
+        buttonEntrance.click();
+        WebElement buttonEnter = driver.findElement(By.xpath(OnlinerPage.buttonEnter));
+        buttonEnter.click();
+        List<WebElement> warningMessagesEmptyCredentials = driver.findElements(
+                By.xpath(OnlinerPage.warningMessagesEmptyCredentials));
+    }
+
+    public void testOnlinerLoginFormWithEmptyPassword() {
+        WebElement buttonEntrance = driver.findElement
+                (By.xpath(OnlinerPage.buttonEntrance));
+        buttonEntrance.click();
+        WebElement inputNickOrEmail = driver.findElement
+                (By.xpath(OnlinerPage.inputNickOrEmail));
+        inputNickOrEmail.sendKeys("test@gmail.com");
+        WebElement buttonEnter = driver.findElement
+                (By.xpath(OnlinerPage.buttonEnter));
+        buttonEnter.click();
+    }
+}
